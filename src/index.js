783e02cfa950d447cf6bf8d4be0247f5f7f713ef
Hatim Damra <hatim.damra@redbox.com>
2019-11-30 22:11:51 -0600

    init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// TODO: have this css file in a link tag in index.html to avoid large output from webpack (or gitignore it)
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.render(
+  <div>Testing</div>,
+  document.getElementById('app')
+)
