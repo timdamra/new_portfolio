2aa7f5107d94a8388fca8e1b249f65bb15b6aa63
Hatim Damra <hatim.damra@redbox.com>
2019-12-07 19:30:08 -0600

    fixed gh-pages img config

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 import CardComponent from './CardComponent'
 import './index.css'
 
-export class Home extends Component {
+export class Home extends Component {    
     render() {
+        console.log(process.env)
         return (
             <main className="home__grid">
                 <CardComponent />
