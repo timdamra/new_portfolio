0db9ad5618f285782e905c4ced3d865f7816511f
Hatim Damra <hatim.damra@redbox.com>
2019-12-08 12:43:09 -0600

    added image src to card

--- src/components/Home/CardComponent.js
+++ src/components/Home/CardComponent.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
+import myPic from '../../../public/images/pic.jpg'
 
-const src = process.env.NODE_ENV === 'production' 
-? 'https://cdn1.imggmi.com/uploads/2019/12/8/d73912e871086fcb903a9b47f0f52c6d-full.jpg' 
-: '/public/images/pic.jpg'
+// const src = process.env.NODE_ENV === 'production' 
+// ? 'https://cdn1.imggmi.com/uploads/2019/12/8/d73912e871086fcb903a9b47f0f52c6d-full.jpg' 
+// : '/public/images/pic.jpg'
 
 export const CardComponent = () => (
   <Card>
-    <Image src={src} wrapped ui={false} />
+    <Image src={myPic} wrapped ui={false} />
     <Card.Content>
       <Card.Header>Hatim Damra</Card.Header>
       <Card.Meta>
