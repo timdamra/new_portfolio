33a7cc2b83f5f65e2ee45f4dc731822aecce13aa
Hatim Damra <hatim.damra@redbox.com>
2019-12-10 15:42:11 -0600

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,8 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
-            filename: './index.html'
+            filename: './index.html',
+            favicon: './public/images/icon.png'
         })
     ],
     resolve: {
