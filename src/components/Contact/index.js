d8970bdbaf7696bf30c1fd101d4be7da17e057f3
Hatim Damra <hatim.damra@redbox.com>
2019-12-23 18:59:46 -0600

    contact page styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,22 +5,69 @@ import './index.css'
 export const Contact = () => {    
     return (
         <section className="contact">
-            <a
-                target="_blank" 
-                rel="noopener noreferrer"
-                href='https://github.com/timdamra'
-            >
-                <i className="github icon"></i>
-                <p>Github</p>
-            </a>
-            <a
-                target="_blank" 
-                rel="noopener noreferrer"
-                href='https://www.linkedin.com/in/hatimdamra/'
-            >
-                <i className="linkedin icon"></i>
-                <p>LinkedIn</p>
-            </a>
+            <div>
+                <div>
+                    <a
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        href='https://github.com/timdamra'
+                        className='contact--icon'
+                    >
+                        <i className="github icon"></i>                    
+                    </a>
+                    <p>Github</p>
+                </div>
+                <a
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    href='https://github.com/timdamra'
+                    className='contact--link'
+                >
+                    github.com/timdamra
+                </a>
+            </div>
+            <div>
+                <div>
+                    <a
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        href='https://www.linkedin.com/in/hatimdamra/'
+                        className='contact--icon'
+                    >
+                        <i className="linkedin icon"></i>                    
+                    </a>
+                    <p>LinkedIn</p>
+                </div>
+                <a
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    href='https://www.linkedin.com/in/hatimdamra/'
+                    className='contact--link'
+                >
+                    linkedin.com/in/hatimdamra
+                </a>
+            </div>
+            <div>
+                <div>
+                    <a
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        href='mailto:hdamra@gmail.com'
+                        className='contact--icon'
+                    >
+                        <i className="google icon"></i>                    
+                    </a>
+                    <p>Email</p>
+                </div>
+                <a
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    href='mailto:hdamra@gmail.com'
+                    className='contact--link'
+                >
+                    hdamra@gmail.com                    
+                </a>
+            </div>
         </section>
     )
 }
