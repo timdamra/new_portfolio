a2b4d8adc6e71bb0714b884cef0979a2c32aa80d
timdamra <hdamra@gmail.com>
2020-01-06 20:05:11 -0600

    fixed my works page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,54 +4,55 @@ import './index.css'
 
 const MyImage = lazy(() => import('./img'))
 
-export const Contact = () => {    
-    return (
-        <section className="contact">
-            <div>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <MyImage />
-                </Suspense>
-            </div>
-            <div>
-                <span className="contact__span-block">
-                    <a
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        href='https://github.com/timdamra'
-                        className='contact--icon'
-                        title="Github"
-                    >
-                        <i className="github icon"></i>                    
-                    </a>                        
-                </span>
-                <span className="contact__span-block">
-                    <a
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        href='https://www.linkedin.com/in/hatimdamra/'
-                        className='contact--icon'
-                        title="LinkedIn"
-                    >
-                        <i className="linkedin icon"></i>                    
-                    </a>                        
-                </span>
-                <span className="contact__span-block">
-                    <a
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        href='mailto:hdamra@gmail.com'
-                        className='contact--icon'
-                        title="Gmail"
-                    >
-                        <i className="google icon"></i>                    
-                    </a>
-                </span>
-                <p>Please feel free to contact me about any opportunities you want to discuss, feedback you want me to hear or 
-                    just to say hi, Cheers!
-                </p>
-            </div>
-        </section>
-    )
+export const Contact = () => {
+  return (
+    <section className="contact">
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <MyImage />
+        </Suspense>
+      </div>
+      <div>
+        <span className="contact__span-block">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/timdamra"
+            className="contact--icon"
+            title="Github"
+          >
+            <i className="github icon"></i>
+          </a>
+        </span>
+        <span className="contact__span-block">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/hatimdamra/"
+            className="contact--icon"
+            title="LinkedIn"
+          >
+            <i className="linkedin icon"></i>
+          </a>
+        </span>
+        <span className="contact__span-block">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:hdamra@gmail.com"
+            className="contact--icon"
+            title="Gmail"
+          >
+            <i className="google icon"></i>
+          </a>
+        </span>
+        <p>
+          Please feel free to contact me about any opportunities you want to
+          discuss, cheers!
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,72 +5,59 @@ import { PORTFOLIO_CONTENT } from '../content'
 import { useGlobalState } from 'state'
 import { ADD_OPEN_PROJECT, REMOVE_OPEN_PROJECT } from 'actions'
 
-import bitcoin from 'public/images/bitcoin.png'
 import './index.css'
 
 const CardComponent = lazy(() => import('../UI/CardComponent'))
 
 export const Portfolio = () => {
-    const param = useParams()[0]
-    const { dispatch } = useGlobalState() 
-    const {
-        crypto: { 
-            header: cryptoHeader, 
-            link: cryptoLink, 
-            labelList: cryptoList
-        },
-        books: { 
-            header: booksHeader, 
-            link: booksLink, 
-            labelList: booksList
-        },
-        tictactoe: {
-            header: ticTacHeader,
-            link: ticTacLink,
-            labelList: ticTacList
-        }
-    } = PORTFOLIO_CONTENT
+  const param = useParams()[0]
+  const { dispatch } = useGlobalState()
+  const {
+    crypto: { header: cryptoHeader, link: cryptoLink, labelList: cryptoList },
+    books: { header: booksHeader, link: booksLink, labelList: booksList },
+    tictactoe: { header: ticTacHeader, link: ticTacLink, labelList: ticTacList }
+  } = PORTFOLIO_CONTENT
 
-    React.useEffect(() => {
-        dispatch({ type: ADD_OPEN_PROJECT, payload: param })
+  React.useEffect(() => {
+    dispatch({ type: ADD_OPEN_PROJECT, payload: param })
 
-        return () => dispatch({ type: REMOVE_OPEN_PROJECT })
-    }, [param])        
+    return () => dispatch({ type: REMOVE_OPEN_PROJECT })
+  }, [param])
 
-    return (
-        <section className="portfolio">
-            <div className="portfolio__project">                
-                <Suspense fallback={<div>Loading...</div>}>
-                    <CardComponent 
-                        pic={bitcoin}
-                        header={cryptoHeader}
-                        labelList={cryptoList}
-                        link={cryptoLink}                        
-                    />
-                </Suspense>                  
-            </div>
-            <div className="portfolio__project portfolio__icon--link">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <CardComponent 
-                        imgIcon='book icon'
-                        header={booksHeader}
-                        labelList={booksList}
-                        link={booksLink}                        
-                    />
-                </Suspense> 
-            </div>  
-            <div className="portfolio__project portfolio__icon--link">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <CardComponent 
-                        imgIcon='game icon'
-                        header={ticTacHeader}
-                        labelList={ticTacList}
-                        link={ticTacLink}
-                    />
-                </Suspense>    
-            </div>         
-        </section>
-    )
+  return (
+    <section className="portfolio">
+      <div className="portfolio__project portfolio__icon--link">
+        <Suspense fallback={<div>Loading...</div>}>
+          <CardComponent
+            imgIcon="bitcoin icon"
+            header={cryptoHeader}
+            labelList={cryptoList}
+            link={cryptoLink}
+          />
+        </Suspense>
+      </div>
+      <div className="portfolio__project portfolio__icon--link">
+        <Suspense fallback={<div>Loading...</div>}>
+          <CardComponent
+            imgIcon="book icon"
+            header={booksHeader}
+            labelList={booksList}
+            link={booksLink}
+          />
+        </Suspense>
+      </div>
+      <div className="portfolio__project portfolio__icon--link">
+        <Suspense fallback={<div>Loading...</div>}>
+          <CardComponent
+            imgIcon="game icon"
+            header={ticTacHeader}
+            labelList={ticTacList}
+            link={ticTacLink}
+          />
+        </Suspense>
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio
